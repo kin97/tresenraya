1f759094a8a4ee930957d5cc3a0e66af978dd51b
kin97 <oscarcasal1997@gmail.com>
2018-04-30 18:57:28 +0200

    Estado inicial

---
+++ js/tresenraya.js
@@ -0,0 +1,66 @@
+window.onload = function () {
+    var el = document.getElementById("body");
+    el.setAttribute("style", "height:" + window.innerHeight + "px");
+
+    var celdas = document.getElementsByClassName("celda");
+
+    var turno = true
+    var juego=["","","","","","","","",""]
+    for (var celda of celdas) {
+        celda.onclick = function () {
+            if (turno) {
+                this.innerHTML = "X"
+                turno = false;
+                juego[this.id]="X"
+            } else {
+                this.innerHTML = "O"
+                turno = true;
+                juego[this.id]="O"
+            }
+            comprobar(juego)
+        }
+    }
+}
+function comprobar(juego){
+    if(juego[0]==juego[1]&&(juego[1]==juego[2])&&(juego[0]!="")){
+        alert("ha ganado: "+juego[0])
+        Vaciar()
+    }
+    if(juego[0]==juego[3]&&(juego[3]==juego[6])&&(juego[0]!="")){
+        alert("ha ganado: "+juego[0])
+        Vaciar()
+    }
+    if(juego[0]==juego[4]&&(juego[4]==juego[8]&&juego[0]!="")){
+        alert("ha ganado: "+juego[0])
+        Vaciar()
+    }
+    if(juego[3]==juego[4]&&(juego[4]==juego[5])&&(juego[3]!="")){
+        alert("ha ganado: "+juego[3])
+        Vaciar()
+    }
+    if(juego[6]==juego[7]&&(juego[7]==juego[8])&&(juego[6]!="")){
+        alert("ha ganado: "+juego[6])
+        Vaciar()
+    }
+    if(juego[1]==juego[4]&&(juego[4]==juego[7])&&(juego[1]!="")){
+        alert("ha ganado: "+juego[1])
+        Vaciar()
+    }
+    if(juego[2]==juego[5]&&(juego[5]==juego[8])&&(juego[2]!="")){
+        alert("ha ganado: "+juego[2])
+        Vaciar()
+    }
+    if(juego[2]==juego[4]&&(juego[4]==juego[6])&&(juego[2]!="")){
+        alert("ha ganado: "+juego[2]);
+        Vaciar()
+    }
+}
+
+
+function Vaciar(){
+    var celdas = document.getElementsByClassName("celda");
+    for(let c of celdas) {
+        c.innerHTML="";
+      var  juego=["","","","","","","","",""]
+    }
+}
